57fe511eddba12f0cf29c1f53f3ee7cd7700b04a
somratcste <somratcste@gmail.com>
2018-12-10 16:29:58 +0600

    query

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
-
-
-
+select count(*) from mysql.user;
+select count(*) from items where location = "New York";
+select count(*) from (select * from categories group by item_id having count(*) = 4 ) C;
 
 select count(*) from sellers where rating > 1000;
 select count(*) from sellers inner join bidders on sellers.user_id = bidders.user_id;
-select count(*) from items inner join categories on items.item_id = categories.item_id where items.number_of_bids >= 1 and currently > 100;
+select count(*) from (select count(*) from categories inner join bids on categories.item_id = bids.item_id where bids.amount > 100 group by categories.category having count(*) >= 1) C;
